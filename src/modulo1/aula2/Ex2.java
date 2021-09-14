c9322be863f4fb59d1194ae3a6b5ebc226518f0b
Raissa M. Lima <raissamartinsarq@gmail.com>
2021-09-14 11:55:01 -0300

    Exercício professor 02

---
+++ src/modulo1/aula2/Ex2.java
@@ -0,0 +1,24 @@
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+class a1_console {
+    public a1_console() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Olá");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o seu nome: ");
+        String nome = scanner.next();
+        System.out.print("Digite o seu sobrenome: ");
+        String sobrenome = scanner.next();
+        System.out.print("Digite a sua idade: ");
+        int idade = scanner.nextInt();
+        if (idade >= 18) {
+            System.out.println("Parabéns, seu cadastro foi realizado com sucesso");
+        }else{
+            System.out.println("Lamentamos, não podemos fazer seu cadastro, visto que, você é menor de idade");
+        }
+    }
+}
